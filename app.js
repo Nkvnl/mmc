66515997f0e9999524b4b6c3553240ec48cf9c45
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-09-12 20:30:15 +0000

    seo

--- app.js
+++ app.js
@@ -10,25 +10,25 @@ app.use(express.static(__dirname + "/public"));
 
 
 
-var app = express(),
-    sitemap = sm.createSitemap({
-        hostname: 'http://www.boo-at-the-zoo.nl',
-        cacheTime: 600000, // 600 sec - cache purge period
-        urls: [
-            { url: '/', changefreq: 'daily', priority: 0.3 },
+// var app = express(),
+//     sitemap = sm.createSitemap({
+//         hostname: 'http://www.boo-at-the-zoo.nl',
+//         cacheTime: 600000, // 600 sec - cache purge period
+//         urls: [
+//             { url: '/', changefreq: 'daily', priority: 0.3 },
 
-        ]
-    });
+//         ]
+//     });
 
-app.get('/sitemap.xml', function(req, res) {
-    sitemap.toXML(function(err, xml) {
-        if (err) {
-            return res.status(500).end();
-        }
-        res.header('Content-Type', 'application/xml');
-        res.send(xml);
-    });
-});
+// app.get('/sitemap.xml', function(req, res) {
+//     sitemap.toXML(function(err, xml) {
+//         if (err) {
+//             return res.status(500).end();
+//         }
+//         res.header('Content-Type', 'application/xml');
+//         res.send(xml);
+//     });
+// });
 app.get("/", function(req, res) {
     res.render("index");
 });
