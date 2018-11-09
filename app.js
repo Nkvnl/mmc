45ab6a0913d6a7058d73df3db36b99463f27c3be
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-11-09 19:18:47 +0000

    new version

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var sanitizeHtml = require('sanitize-html');
 // var seedDB = require("./seeds")
 // seedDB()
 
-mongoose.connect("mongodb://localhost/meditatie");
+mongoose.connect("mongodb://nkvnl:meditatiec0@ds019664.mlab.com:19664/meditatieco");
 app.use(compression());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
